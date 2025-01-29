77dd16ba1417f05a0470cc2539ee6fee57a76d25
ngeor26 <114959096+ngeor26@users.noreply.github.com>
2025-01-29 14:24:39 +0000

    Jan 29, 2025, 9:24 AM

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Deck deck = new Deck();
-        System.out.println(deck.drawCard());
-        System.out.println(deck.drawCard());
+        // System.out.println(deck.drawCard());
+        // System.out.println(deck.drawCard());
         // System.out.println(deck);
 
         Hand hand = new Hand(5, deck);
