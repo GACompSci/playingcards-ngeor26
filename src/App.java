754418e180099d65ae6ec4c94ab82482c41d6e1c
ngeor26 <114959096+ngeor26@users.noreply.github.com>
2025-01-29 09:41:42 -0500

    hand class

--- src/App.java
+++ src/App.java
@@ -9,8 +9,8 @@ public class App {
 
         Hand hand = new Hand(5, deck);
         hand.printHand();
-        System.out.println("drawing");
-        System.out.println(hand.cardPresent(2, 1));
+        System.out.println("playing card");
+        System.out.println(hand.playCard(2, 1));
         System.out.println("sorting");
         hand.sortHand();
         hand.addCard(new Card());
